2655d81ed395d3d1f9f1be880ee835cd283db2b6
BugHunter-X <askzenai@gmail.com>
2024-12-17 14:08:07 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val list = mutableListOf<Int>(1, 2, 3, 4, 5)
+    list.removeIf { it % 2 == 0 }
+    println(list) // Output: [1, 3, 5]
+
+    val list2 = mutableListOf(1, 2, 3, 4, 5)
+    list2.removeAll { it % 2 == 0 }
+    println(list2) // Output: [1, 3, 5]
+
+    val set = mutableSetOf(1, 2, 3, 4, 5)
+    set.removeIf { it % 2 == 0 }
+    println(set) // Output: [1, 3, 5]
+}
